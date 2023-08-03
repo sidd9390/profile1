c5b8aa355517f685a709cce68811d51b047bec4e
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-08-03 22:48:49 +0530

    seconds change

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ let grd = document.getElementById("grd");
 let dip = document.getElementById("dip");
 let school = document.getElementById("school");
 
-function diploma(){
-    grd.style.display = "none";
-    dip.style.display = "block";
-    school.style.display = "none";
+function diploma() {
+  grd.style.display = "none";
+  dip.style.display = "block";
+  school.style.display = "none";
 }
-function schooling(){
-    grd.style.display = "none";
-    dip.style.display = "none";
-    school.style.display = "block";
+function schooling() {
+  grd.style.display = "none";
+  dip.style.display = "none";
+  school.style.display = "block";
 }
-function graduation(){
-    grd.style.display = "block";
-    dip.style.display = "none";
-    school.style.display = "none";
+function graduation() {
+  grd.style.display = "block";
+  dip.style.display = "none";
+  school.style.display = "none";
 }
+
