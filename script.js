0b5beb710f0ef41ac09f512e5fa0467b4fb1582e
Grandhi Siddu <113771456+sidd9390@users.noreply.github.com>
2023-08-10 20:43:24 +0530

    changed

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-alert("siddu");
 let grd = document.getElementById("grd");
 let dip = document.getElementById("dip");
 let school = document.getElementById("school");
-let webProjects = document.getElementById("web-projects");
-let collegeProjects = document.getElementById("college-projects");
+let web_pro = document.getElementById("web-projects");
+let col_pro = document.getElementById("college-projects");
 
+function graduation() {
+  grd.style.display = "block";
+  dip.style.display = "none";
+  school.style.display = "none";
+  document.getElementById("btn-1").style.borderBottomColor = "black";
+  document.getElementById("btn-2").style.borderBottomColor = "transparent";
+  document.getElementById("btn-3").style.borderBottomColor = "transparent";
+}
 function diploma() {
   grd.style.display = "none";
   dip.style.display = "block";
   school.style.display = "none";
+  document.getElementById("btn-1").style.borderBottomColor = "transparent";
+  document.getElementById("btn-2").style.borderBottomColor = "black";
+  document.getElementById("btn-3").style.borderBottomColor = "transparent";
 }
 function schooling() {
   grd.style.display = "none";
   dip.style.display = "none";
   school.style.display = "block";
+  document.getElementById("btn-1").style.borderBottomColor = "transparent";
+  document.getElementById("btn-2").style.borderBottomColor = "transparent";
+  document.getElementById("btn-3").style.borderBottomColor = "black";
 }
-function graduation() {
-  grd.style.display = "block";
-  dip.style.display = "none";
-  school.style.display = "none";
+function function_1() {
+  web_pro.style.display = "flex";
+  col_pro.style.display = "none";
+  document.getElementById("btn-4").style.borderBottomColor = "black";
+  document.getElementById("btn-5").style.borderBottomColor = "transparent";
 }
-function webProjects(){
-  webProjects.style.display = "flex";
-  collegeProjects.style.display = "none";
-}
-function collegeProjects(){
-  webProjects.style.display = "none";
-  collegeProjects.style.display = "flex";
+function function_2() {
+  web_pro.style.display = "none";
+  col_pro.style.display = "flex";
+  document.getElementById("btn-4").style.borderBottomColor = "transparent";
+  document.getElementById("btn-5").style.borderBottomColor = "black";
 }
